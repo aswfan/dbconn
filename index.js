bef7b9c1acf81bf9fecf74ad33d39e08c0603495
fan <yyfan1994@gmail.com>
2018-03-26 22:23:28 +0100

    switch to mssql

--- index.js
+++ index.js
@@ -6,29 +6,45 @@ const DBAddr = process.env.DBADDR || "db:1433";
 const PORT = 80;
 const HOST = "";
 
-const Connection = require("tedious").Connection;
+const mssql = require("mssql");
 const config = {
   userName: "SA",
   password: "GoTeam2018!",
   server: DBAddr,
-  options: {
-    encrypt: true,
-    database: "YVYC"
-  }
+  database: "YVYC",
+  port: 1433
 };
 
-var connection = new Connection(config);
+var conn = new mssql.Connection(config);
 
 const app = express();
 
 app.get("/", (req, res) => {
-  connection.on("connect", function(err) {
-    res.send("Hello World! Got connected!\n");
-  });
+  conn
+    .connect()
+    .then(() => {
+      var dbreq = new mssql.Request(conn);
+      res.set("Content-Type", "text/plain");
+      dbreq
+        .query("select * from proposal.draft_proposal")
+        .then(records => {
+          res.send(`Hello World!\n${records}`);
+          dbconn.close();
+        })
+        .catch(err => {
+          console.log(err.stack);
+          res.send(err.message);
+          dbconn.close();
+        });
+    })
+    .catch(err => {
+      console.log(err.stack);
+      res.send(err.message);
+    });
 });
 
-const dbconn = require("./dbconn");
-app.use(dbconn({ connection }));
+// const dbconn = require("./dbconn");
+// app.use(dbconn({ connection }));
 
 app.use((err, req, res, next) => {
   console.log(err.stack);
--- testdb.js
+++ testdb.js
@@ -3,13 +3,13 @@ const DBAddr = process.env.DBADDR || "db:1433";
 
 var Connection = require("tedious").Connection;
 const config = {
-	userName: "SA",
-	password: "GoTeam2018!",
-	server: DBAddr,
-	options: {
-	  database: "YVYC"
-	}
-  };
+  userName: "SA",
+  password: "GoTeam2018!",
+  server: DBAddr,
+  options: {
+    database: "YVYC"
+  }
+};
 var connection = new Connection(config);
 connection.on("connect", function(err) {
   // If no error, then good to proceed.
