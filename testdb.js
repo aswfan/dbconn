4482e9ab2a2b904073d38585a205e849eb7538fc
fan <yyfan1994@gmail.com>
2018-03-26 21:51:37 +0100

    test db connection

---
+++ testdb.js
@@ -0,0 +1,17 @@
+"use strict";
+const DBAddr = process.env.DBADDR || "db:1433";
+
+var Connection = require("tedious").Connection;
+const config = {
+	userName: "SA",
+	password: "GoTeam2018!",
+	server: DBAddr,
+	options: {
+	  database: "YVYC"
+	}
+  };
+var connection = new Connection(config);
+connection.on("connect", function(err) {
+  // If no error, then good to proceed.
+  console.log("Connected");
+});
