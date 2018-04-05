e2fd90b8454f80fd7b9276631af36f5685e85247
fan <yyfan1994@gmail.com>
2018-04-05 11:41:49 -0700

    export draft

--- draftmgt.js
+++ draftmgt.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const express = require("express");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
 module.exports = db => {
   let handler = (res, qsql) => {
@@ -16,7 +16,6 @@ module.exports = db => {
 
   router.use(bodyParser.json()); // support json encoded bodies
   router.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
-  
 
   router.get("/", (req, res) => {
     res.send("hello draft management!");
@@ -28,6 +27,66 @@ module.exports = db => {
     handler(res, qsql);
   });
 
+  router.get("/export", (req, res) => {
+    var qsql = "select * from proposal.draft_proposal";
+    var handler = recordset => {
+      var nodeExcel = require("excel-export");
+      var conf = {};
+      conf.cols = [
+        {
+          caption: "proposal_id",
+          type: "number",
+          width: 3
+        },
+        {
+          caption: "final_proposal_title",
+          type: "string",
+          width: 50
+        },
+        {
+          caption: "final_proposal_idea",
+          type: "string",
+          width: 50
+        },
+        {
+          caption: "final_proposal_location",
+          type: "string",
+          width: 50
+        },
+        {
+          caption: "final_proposal_latitude",
+          type: "number",
+          width: 10
+        },
+        {
+          caption: "final_proposal_longitude",
+          type: "number",
+          width: 10
+        }
+      ];
+
+      let rows = recordset["recordset"];
+      let arr = [];
+      for (var i = 0; i < rows.length; i++) {
+        let row = rows[i];
+        console.log(row["proposal_longitude"]);
+        let a = [row["draft_id"], row["proposal_title"], row["proposal_idea"], row["project_location"], row["proposal_latitude"], row["proposal_longitude"]];
+        arr.push(a);
+      }
+      conf.rows = arr;
+      var result = nodeExcel.execute(conf);
+      res.setHeader("Content-Type", "application/vnd.openxmlformates");
+      res.setHeader(
+        "Content-Disposition",
+        "attachment;filename=" + "drafts.xlsx"
+      );
+      res.end(result, "binary");
+      console.log(recordset);
+    };
+
+    db(qsql, handler);
+  });
+
   router.get("/:id", (req, res) => {
     let qsql = `select * from proposal.draft_proposal where draft_id=${
       req.params.id
@@ -35,11 +94,6 @@ module.exports = db => {
 
     handler(res, qsql);
   });
-
-  router.post("/add", (req, res) => {
-    var user_id = req.body.id;
-    console.log(user_id);
-  })
 
   return router;
 };
