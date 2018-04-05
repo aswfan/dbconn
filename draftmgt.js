7b40e9c42fea1fa44d9b370a6b63edfa1f624841
fan <yyfan1994@gmail.com>
2018-04-05 04:13:09 -0700

    create draft mgt

--- draftmgt.js
+++ draftmgt.js
@@ -2,31 +2,33 @@
 
 const express = require("express");
 
-const selecthandler = res => {
-  recordset => {
-    res.set("Content-Type", "text/plain");
-    res.send(recordset["recordsets"]);
-  };
-};
-
 module.exports = db => {
+  let handler = (res, qsql) => {
+    let handler = recordset => {
+      console.log(recordset);
+      res.json(recordset["recordsets"]);
+    };
+    db(qsql, handler);
+  };
+
   let router = express.Router();
   router.get("/", (req, res) => {
     res.send("hello draft management!");
   });
 
   router.get("/all", (req, res) => {
-    var qsql = `select * from proposal.draft_proposal`;
-    db(qsql, selecthandler(res));
+    let qsql = `select * from proposal.draft_proposal`;
+
+    handler(res, qsql);
   });
 
-  //   router.get("/:id", (req, res) => {
-  //     var qsql = `select * from proposal.draft_proposal where draft_id=${
-  //       req.params.id
-  //     }`;
-  //     db(qsql, selecthandler(res));
-  //     // res.send(`hello ${req.params.id}!`);
-  //   });
+  router.get("/:id", (req, res) => {
+    let qsql = `select * from proposal.draft_proposal where draft_id=${
+      req.params.id
+    }`;
+
+    handler(res, qsql);
+  });
 
   return router;
 };
