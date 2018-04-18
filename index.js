fd84beccb620b9716710f1100920bb18890b7c13
736038700@qq.com <736038700@qq.com>
2018-04-18 15:42:45 -0700

    add bash file

--- index.js
+++ index.js
@@ -6,10 +6,9 @@ const HOST = "";
 const express = require("express");
 const app = express();
 
-const cors = require("cors");
-
 const db = require("./dbconn");
 
+const cors = require("cors");
 app.use(cors());
 
 app.get("/", (req, res) => {
